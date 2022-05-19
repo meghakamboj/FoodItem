05fe23e9137944a6b2b6681816a184d842884f25
meghakamboj <97784499+meghakamboj@users.noreply.github.com>
2022-05-19 18:04:46 +0530

    Add files via upload

---
+++ FoodItem/FoodItem/FoodItem/Controllers/FoodItemController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodItemWebAPI.services;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoodItemController : ControllerBase
+    {
+
+        private readonly IFoodItemService _foodItemService;
+        public FoodItemController(IFoodItemService foodItemService)
+        {
+            _foodItemService = foodItemService;
+        }
+        [HttpGet]
+        [Route("GetFoodItems/")]
+        public async Task<ActionResult<IEnumerable<FoodItems>>> GetFoodItems()
+        {
+            return (await _foodItemService.GetFoodItems()).ToList();
+        }
+
+
+
+    }
+}
---
+++ FoodItem/FoodItem/FoodItem/services/IFoodItemService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestaurantAPI.Models;
+
+namespace FoodItemWebAPI.services
+{
+    public interface IFoodItemService
+    {
+        Task<IEnumerable<FoodItems>> GetFoodItems();
+
+    }
+}
